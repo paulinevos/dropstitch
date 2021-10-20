2b55808f77b5b2b9501cdfb76b78911c322146e1
Pauline Vos <pauline.vos@werkspot.nl>
2021-10-20 13:51:30 +0200

    Undo rebase
    Dropstitch can now find the user's most recent rebase and reset it for
    them! 🎉

--- dropstitch.sh
+++ dropstitch.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-ref_regex="^(([a-z]|[0-9]){7}) HEAD@{[0-9]+}: (rebase|reset):? .+$"
-moves=(rebase reset)
-
+rebase_regex="^([a-z]|[0-9]){7} HEAD@{[0-9]+}: rebase \(start\).+$"
+ref_id_regex="^(([a-z]|[0-9]){7}).+$"
 
 if ! git rev-parse --git-dir > /dev/null 2>&1; then
-    echo "You are not inside a Git repo."
+    echo "You are not inside a Git repository."
     exit 1
 fi
 
+reset=false
+
+confirm () {
+    read -p "Undo $1? [y/n]: " input </dev/tty
+
+    if [[  $input =~ ^y|yes$ ]]
+    then
+        echo "Resetting to $2"
+        reset_to_ref $2
+    fi
+    reset=false
+}
+
+reset_to_ref () {
+    git reset --hard $1
+}
 
 git reflog | while read line
 do
-    if [[ $line =~ $ref_regex ]]
+    if [[ $reset != false ]]
     then
-        move=`echo $line | sed -E "s/$ref_regex|.*/\3/"`
-        ref=`echo $line | sed -E "s/$ref_regex|.*/\1/"`
-        echo $move $ref
+        ref_id=`echo $line | sed -E "s/$ref_id_regex|.*/\1/"`
+        confirm "rebase" $ref_id
+    else
+        if [[ $line =~ $rebase_regex ]]
+        then
+            echo "Detected a recent rebase."
+            reset=true
+        fi
     fi
 done
-
-find_ref_id () {
-    return echo $1 | sed -E "s/$ref_regex|.*/\1/"
-}
-
-in_array () {
-    for i in "${2[@]}"
-    do
-        if [ "$i" = "$1" ] ; then
-            return true
-        fi
-    done
-    return false
-}
